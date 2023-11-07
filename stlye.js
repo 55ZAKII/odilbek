59b3b45847ee0877d340f1fc60fcd6249b1e64bf
55zakii <zodilbek2009@gmail.com>
2023-11-07 18:32:49 +0500

    odilbek commit

--- stlye.js
+++ stlye.js
@@ -34,8 +34,8 @@
 //   name:'xasan',
 //   age: 22,
 // }
-// const {name} = user
-// console.log(name);
+// const {age} = user
+// console.log(age);
 
 ///////////////////////////////////////////////////////////
 
@@ -95,27 +95,99 @@
 //     }
 //      },'1000')
 // }
+//
+//   window.addEventListener('load',function(){
+//     localStorage.setItem('black',JSON.stringify(dataBase))
+// });
+// res =localStorage.getItem('black') 
+// console.log(res,'res');
+//
 // login('muhammadali',1234,(usr) => {
 //     console.log(usr);
 //     console.log('finished');
 // })
-
+//
 // ////////////////////////////////////////////////////////////////////////////////
 
-function divide(a, b) {
-    try{
-        if (b === 0) {
-            throw new Error("Nolga bo'lish mumkin emas")
-        }
-        return a / b;
-    }catch (error) {
-        console.error("xatolik yuzaga keldi", error.message);
-        return null;
-    }
+// function divide(a, b) {
+//     try{
+//         if (b === 0) {
+//             throw new Error("Nolga bo'lish mumkin emas")
+//         }
+//         return a / b;
+//     }catch (error) {
+//         console.error("xatolik yuzaga keldi", error.message);
+//         return null;
+//     }
+// }
+
+
+// const result1 = divide(7454353423,4545234); //Bo'lish natijasi: 4
+// const result2 = divide(100,2); //Bo'lish natijasi: null
+
+// console.log("natija 1:", result1);
+// console.log("natija 2:", result2);
+
+///////////////////////////////////////////////////////////////////
+
+// console.log('loding...');
+// function delayGeeting(deley, isHappy) {
+//     return new Promise((resolve, reject) =>{
+//         setTimeout(() =>{
+//             if (isHappy) {
+//                 resolve('Xush kelibsiz😊')
+//             }else{
+//                 reject('salom👋')
+//             }
+//         }, deley)
+//     });
+// }
+
+// delayGeeting(2000, false)
+//     .then(massege =>{
+//         console.log(massege); //'Xush kelibsiz😊'
+//     })
+//     .catch(massege =>{
+//         console.log(massege);//'salom👋'
+//     })
+
+/////////////////////////////////////////////////////////////////////
+
+// const studentList =document.getElementById("studentList");
+// const studentListPlus = document.getElementById('studentList')
+// const studentUL = document.getElementById("studentUL");
+// const studentInput = document.getElementById("newstudent");
+// const addstudentButton = document.getElementById("addstudentButton");
+
+// const stdunth2 = document.getElementsByTagName('h2')
+// console.log(stdunth2, 'h2');
+// stdunth2[0].style.color = 'red'
+// stdunth2[0].style.fontSize = '50px'
+
+// const InputOne =document.querySelector('#one')
+
+// //yangi talabani qoshish
+
+// addstudentButton.addEventListener("click",function () {
+//     const newStudentName = InputOne.value;
+//     console.log(newStudent,'newStudent');
+//     if (newStudentName) {
+//     const li =document.createElement("li")
+//     li.textContent = newStudentName;
+//     studentUL.appendChild(li);
+//     newStudentInput.value = "";
+//     }
+// });
+
+//////////////////////////////////////////////////////////////////////
+
+const a =[4354,[5435435345,543534545345454]]
+
+const arrey =(a) => {
+    return a
 }
 
-const result1 = divide(,2); //Bo'lish natijasi: 4
-const result2 = divide(100,2); //Bo'lish natijasi: null
+console.log(arrey(a));
 
-console.log("natija 1:", result1);
-console.log("natija 2:", result2);
+///////////////////////////////////////////////////////////////////////
+
