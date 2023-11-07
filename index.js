473bc32cb77fb87681213789ca4847702e516ffe
55zakii <zodilbek2009@gmail.com>
2023-11-07 18:45:18 +0500

    odilbek commit

---
+++ index.js
@@ -0,0 +1,38 @@
+// var str1 = 'cola'
+// var str2 = 'fanta'
+
+// str3 = str1
+// str1 = str2
+// str2 = str3
+
+// console.log(str2);
+
+// console.log(str1);
+
+///////////////////////////////////////////
+
+// output : var yil = 2020
+// input : 'kabisa yili'
+
+// output : yil = 2017
+// input : 'kabisa yili emas'
+
+// function kobasi(yil) {
+//     if (yil === '2020') {
+//         console.log('kabisa yili');
+//         return 2020;
+//     } else {
+//         console.log('kabisa yili emas');
+//         return yil;
+//     }
+// }
+
+// console.log(kobasi(yil));
+
+/////////////////////////////////////////////
+
+function betterThanAverage(classPoints, yourPoints) {
+
+}
+
+////////////////////////////////////////////////
